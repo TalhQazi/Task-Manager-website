ad6d740063bfa41c5ad82028c966de29e770bbbf
Talha Nizami <talha.nizami01@gmail.com>
2026-04-25 15:37:23 +0500

    Include Pending Bugs count in dashboard stat cards

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -113,7 +113,7 @@ const Dashboard = () => {
       >
 
         <motion.div 
-          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 sm:gap-4 lg:gap-6"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-4 lg:gap-6"
           variants={containerVariants}
         >
           {metrics && [
